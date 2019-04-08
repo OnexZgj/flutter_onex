f3078860a76c33b35450fdecdd767b2bb47542d2
onexzgj <474835552@qq.com>
2019-04-08 22:58:53 +0800

    首页banner 开发完成

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,20 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_onex/manager/api_manager.dart';
 
-class TravelPage extends StatefulWidget{
-
+class TravelPage extends StatefulWidget {
   @override
   _travelPage createState() => _travelPage();
-
 }
 
-class _travelPage extends State<TravelPage>{
+class _travelPage extends State<TravelPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    _getArtilcle();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body:new Center(
-        child: Text("TravelPage"),
-      )
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, index) {
+          return _buildRow(index);
+        });
+  }
+
+  Widget _buildRow(int) {
+    return new ListTile(
+      title: new Text(""),
     );
+  }
+
+  void _getArtilcle() async {
+    Response response = await ApiManager().getWechatArticle(408, 1);
   }
 }
